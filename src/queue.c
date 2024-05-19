801832c2e4ab5d3fcc96040d75f027638166ac79
Nguyen Truyen Tuan <tuan160404@gmail.com>
2024-05-20 00:32:20 +0700

    Udapte scheduling module

--- src/queue.c
+++ src/queue.c
@@ -1,47 +1,56 @@
-#include "queue.h"
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "queue.h"
 
-int empty(struct queue_t *q) {
-  if (q == NULL) return 1;
+int empty(struct queue_t *q)
+{
+  if (q == NULL)
+    return 1;
   return (q->size == 0);
 }
 
-void enqueue(struct queue_t *q, struct pcb_t *proc) {
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
   /* TODO: put a new process to queue [q] */
-  #ifdef MLQ_SCHED
-  if (q != NULL && q->size < MAX_QUEUE_SIZE) {
-      q->proc[q->size] = proc;
-      ++q->size;
+  if (proc == NULL) return;
+  if (q -> size == MAX_QUEUE_SIZE) return;
+  int i;
+  // Find the correct position to insert the new process based on its priority
+  for (i = 0; i < q->size; i++)
+  {
+    if (q->proc[i]->priority <= proc->priority)
+    {
+      int j;
+      // Shift all processes with lower or equal priority to the right
+      for (j = q->size - 1; j >= i; j--)
+      {
+        q->proc[j + 1] = q->proc[j];
+      }
+      // Insert the new process at the correct position
+      q->proc[i] = proc;
+      q->size++;
+      return;
+    }
   }
-  #else
-  for (int i = 0; i < q->size; ++i) {
-    if (q->proc[i]->priority <= proc->priority) {
-      for (int j = q->size - 1; j >= i; --j)
-         q->proc[j + 1] = q->proc[j];
-
-         q->proc[i] = proc;
-         ++q->size;
-         return;
-     }
-  }
-
+  // If the new process has lower priority than all existing processes, insert it at the end
   q->proc[q->size] = proc;
-  ++q->size;
-  #endif
+  q->size++;
 }
 
-struct pcb_t *dequeue(struct queue_t *q) {
-  /* TODO: return a pcb whose prioprity is the highest
+struct pcb_t *dequeue(struct queue_t *q)
+{
+  /* TODO: return a pcb whose priority is the highest
    * in the queue [q] and remember to remove it from q
-   * */
-  if (empty(q)) return NULL;
+   */
+  if (empty(q))
+  {
+    return NULL; // Return NULL if the queue is empty
+  }
 
-  struct pcb_t *p = q->proc[0];
-  for (int i = 0; i + 1 < q->size; ++i) q->proc[i] = q->proc[i + 1];
-
+  struct pcb_t *ret = q->proc[0];
+  for (int i = 0; i < (q->size - 1); i++)
+    q->proc[i] = q->proc[i + 1];
   q->proc[q->size - 1] = NULL;
-  --q->size;
-  return p;
+  q->size--;
+  return ret;
 }
